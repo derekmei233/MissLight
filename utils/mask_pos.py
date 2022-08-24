ec7920d5ef7f8a59564af8b2a7c1e90f77ac7100
lijunxian111 <88318195+lijunxian111@users.noreply.github.com>
2022-08-24 13:20:01 +0800

    Update mask_pos.py

--- utils/mask_pos.py
+++ utils/mask_pos.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils.preparation import build_relation
+from preparation import build_relation
 import random
 
 
@@ -8,59 +8,51 @@ def random_mask(num, policy, relation):
     seed = random.seed(random.randint(1, 1000))
     neighbor = relation['neighbor_idx']
     n_intersections = len(neighbor)
-    candidate = set(np.arange(0, n_intersections))
-    neighbor_pool = set()
-    neighbor_non_pool = set(candidate)
-    sampled = set()
+    candidate = list(np.arange(0, n_intersections))
+    neighbor_pool = list()
+    neighbor_non_pool = list(candidate)
+    sampled = list()
 
-    if policy == "allowed":
-        neighbor_flag = 0 
+    if policy == "neighbor":
+        neighbor_flag = 0
         count = 0
         assert num < n_intersections
         while (count < num):
             if (neighbor_flag == 0) and (count == num - 1):
                 tmp = random.choice(neighbor_pool)
-                sampled.add(tmp)
-                candidate.pop(tmp)
+                sampled.append(tmp)
+                candidate.remove(tmp)
             else:
                 tmp = random.choice(candidate)
-                sampled.add(tmp)
-                candidate.pop(tmp)
+                #print(tmp)
+                sampled.append(tmp)
+                candidate.remove(tmp)
                 if tmp in neighbor_pool:
                     neighbor_flag = 1
                 new_neighbor = neighbor[tmp]
                 neighbor_pool = neighbor_pool or new_neighbor
-                neighbor_non_pool.pop(new_neighbor)
+                for item in new_neighbor:
+                    if item in neighbor_non_pool:
+                       neighbor_non_pool.remove(item)
+            count+=1
 
 
-    elif policy == "forbidden":
-        assert num < 7
+    elif policy == "non_neighbor":
+        assert num <= n_intersections/2
         count=0
-        masks=[]
+        #print(neighbor)
         while(count<num):
-            tmp=random.randint(0,15)
-            if tmp+1 not in masks and tmp-1 not in masks and tmp not in masks and tmp+4 not in masks and tmp-4 not in masks:
-                #print(tmp)
-                masks.append(tmp)
-                count+=1
-    return masks
-
-def random_mask(num, policy):
-    random.seed(random.randint(1, 1000))
-    if policy == "allowed":
-        assert num < 16
-        masks = random.sample(range(0,16),num)
-    elif policy == "forbidden":
-        assert num < 7
-        count=0
-        masks=[]
-        while(count<num):
-            tmp=random.randint(0,15)
-            if tmp+1 not in masks and tmp-1 not in masks and tmp not in masks and tmp+4 not in masks and tmp-4 not in masks:
-                #print(tmp)
-                masks.append(tmp)
-                count+=1
-    return masks 
+            tmp=random.choice(candidate)
+            sampled.append(tmp)
+            candidate.remove(tmp)
+            for item in neighbor[tmp]:
+                if item in candidate:
+                   candidate.remove(item)
+            #assert num-count<=len(candidate),"please try a lower num"
+            #tmp=random.randint(0,15)
+            count+=1
+            assert num - count <= len(candidate), "please try a lower num"
+    return sampled
 
 
 if __name__ == '__main__':
@@ -68,4 +60,4 @@ if __name__ == '__main__':
     config_file = 'cityflow_hz4x4.cfg'
     world = World(config_file, thread_num=8)
     relation = build_relation(world)
-    mask = random_mask(2, 'allowed', relation)
+    mask = random_mask(2, 'neighbor', relation)
